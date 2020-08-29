fc469603e5ec3634ccd1fcead1515d260a6a1633
Jasmine Terry <jasmine-terry91@gmail.com>
2020-08-29 16:15:17 -0400

    srever initial code. server runs

--- src/index.js
+++ src/index.js
@@ -5,7 +5,10 @@ import App from './App';
 import * as serviceWorker from './serviceWorker';
 import {BrowserRouter as Router} from 'react-router-dom';
 
+import axios from 'axios';
+
 ReactDOM.render(
+
   <Router>
     <App />
   </Router>,
