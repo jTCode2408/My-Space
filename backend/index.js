e7c5c087a6092f7ce8024ec98daefd599ab22422
Jasmine Terry <jasmine-terry91@gmail.com>
2020-09-03 17:21:29 -0400

    env added, backend server code adjusted

--- backend/index.js
+++ backend/index.js
@@ -1,6 +1,6 @@
 const server = require ('./server')
-const PORT = 4000 || process.env.PORT
+const PORT = 8888 || process.env.PORT
 
 server.listen(PORT, ()=>{
-    console.log(`server running on port ${PORT}`)
+    console.log(`Listening on port ${PORT}. /login to initiate authentication flow.`)
 })
