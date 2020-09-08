d35bd3f10781b1ac000601c2671a8670c321bb8e
Jasmine Terry <jasmine-terry91@gmail.com>
2020-09-08 18:12:02 -0400

    local storage added

--- src/components/Playlist.js
+++ src/components/Playlist.js
@@ -2,22 +2,24 @@ import React, {useState} from 'react';
 import axios from 'axios';
 import {Link} from 'react-router-dom';
 //BACKEND: https://my-space-backend.herokuapp.com/
+import {useLocalStorage} from './utils';
 
-const Playlist =()=>{
+const Playlist =(key, initialVal)=>{
 
     const [music, setMusic]=useState();
-    const [user,setUser]=useState();
+    const [user,setUser]=useLocalStorage(key, initialVal);
 
     axios.get('https://my-space-backend.herokuapp.com/login')
     .then(res=>{
         console.log(res)
+        
         //setUser()
     })
     .catch(err=>{
         console.log(err)
         alert("unable to log in, try again soon!");
     })
-
+  
     return(
         <div>
            
---
+++ src/components/utils.js
@@ -0,0 +1,18 @@
+import {useState} from 'react';
+
+export const useLocalStorage=(key, initialVal)=>{
+    const [storedValue, setStoredValue]=useState(()=>{
+        //usestate holding a fn
+        const auth = window.localStorage.getItem(key);
+        return auth ? JSON.parse(auth) : initialVal;
+    })
+
+
+const setValue= val=>{
+    setStoredValue(val)
+    window.localStorage.setItem(key, JSON.stringify(val))
+}
+
+return [storedValue, setValue];
+
+}
