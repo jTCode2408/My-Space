686dbcfb3ae23f4e88ca1d275fe8372833a0a041
Jasmine Terry <jasmine-terry91@gmail.com>
2020-10-19 23:32:35 -0400

    updating styles for playlists

--- src/components/styles.js
+++ src/components/styles.js
@@ -5,6 +5,7 @@ border:2px solid red;
 display:flex;
 flex-wrap:wrap;
 margin:0 auto;
+align-items:center;
 `
 
 
@@ -16,6 +17,7 @@ export const PlaylistItems=styled.div`
 
 width:100%;
 margin:0 auto;
+
 `
 
 export const Player=styled.div`
@@ -28,5 +30,7 @@ export const PlayerItems=styled.div`
 border:2xp solid orange;
 display:flex;
 flex-direction:column;
-margin:2.5%
+margin:2.5%;
+border:2px solid black;
+align-items:center;
 `
